b6cd7924f214afccfb94b5baa397cfd4c31c610a
Abhisek-Ray99 <rickyrayabhisek99@gmail.com>
2020-11-30 22:41:57 +0530

    update the contact page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -4,36 +4,35 @@ import './style.css';
 function Contact() {
     return (
         <div className="head-contact">
-          <div class="row">
+            <div className="row">
+                <div>
+                    <h2 className="contact-title">Get in Touch</h2>
+                    <hr class="hr-1" />
+                </div>
+
+                <div>
                     <div>
-                        <h2 class="contact-title">Get in Touch</h2>
+                        <div>
+                            <textarea className="contact-message" name="message" cols="120" rows="9" placeholder="Enter Message"></textarea>
+                        </div>
                     </div>
-                        <form>
-                            <div>
-                                <div>
-                                    <div>
-                                        <textarea  className="contact-message"  name="message" cols="125" rows="9" placeholder="Enter Message"></textarea>
-                                    </div>
-                                </div>
-                                <div className="contact-info">
-                                    <div>
-                                        <input className="info-name" name="name" id="name" type="text" placeholder="Enter your name" />
-                                    </div>
-                                    <div>
-                                        <input className="info-email"  name="email" id="email" type="email" placeholder="Enter email address" />
-                                    </div>
-                                </div>
-                                <div className="contact-sub">
-                                    <div>
-                                        <input className="sub-input" name="subject" id="subject" type="text" placeholder="Enter Subject" />
-                                    </div>
-                                </div>
-                            </div>
-                            <div>
-                                <button className="contact-btn" type="submit">Send</button>
-                            </div>
-                        </form>
-          </div> 
+                    <div className="contact-info">
+                        <div>
+                            <input className="info-name" name="name" id="name" type="text" placeholder="Enter your name" />
+                        </div>
+                        <div>
+                            <input className="info-email" name="email" id="email" type="email" placeholder="Enter email address" />
+                        </div>
+                    </div>
+                    <div className="contact-sub">
+                        <div>
+                            <input className="sub-input" name="subject" id="subject" type="text" placeholder="Enter Subject" />
+                        </div>
+                    </div>
+                </div>
+                <button className="contact-btn" type="submit">Send</button>
+
+            </div>
         </div>
     )
 }
